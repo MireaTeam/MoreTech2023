80f80d15f63c9040d8c073f733006b9de4b94d29
Azumayuk1 <azumayuki@outlook.com>
2023-10-14 11:23:43 +0300

    Extended UserLocationObjectListener

--- app/src/main/java/ru/mirea/moretech2023/MainActivity.kt
+++ app/src/main/java/ru/mirea/moretech2023/MainActivity.kt
@@ -3,15 +3,30 @@ package ru.mirea.moretech2023
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.yandex.mapkit.layers.ObjectEvent
+import com.yandex.mapkit.user_location.UserLocationObjectListener
+import com.yandex.mapkit.user_location.UserLocationView
 import ru.mirea.moretech2023.ui.screens.officechoice.YandexMap
 import ru.mirea.moretech2023.ui.screens.welcome.WelcomeScreen
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), UserLocationObjectListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             //WelcomeScreen()
             YandexMap()
         }
+    }
+
+    override fun onObjectAdded(p0: UserLocationView) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onObjectRemoved(p0: UserLocationView) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onObjectUpdated(p0: UserLocationView, p1: ObjectEvent) {
+        TODO("Not yet implemented")
     }
 }
