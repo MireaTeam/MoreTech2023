0e71d7745c52d5df9f92095cc0741a8a5a4c9f20
Azumayuk1 <azumayuki@outlook.com>
2023-10-14 10:35:37 +0300

    Added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
     implementation("androidx.activity:activity-compose:1.8.0")
@@ -58,6 +58,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
 
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material3:material3:1.2.0-alpha09")
